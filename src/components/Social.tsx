0e33f42954e3160bd1bf4691a9c1428b938fac84
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2024-05-26 11:14:10 +0530

    refactor: add aria-labels to social links

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -2,9 +2,17 @@ import Link from "next/link";
 import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
 
 const socialItems = [
-  { icon: <FaGithub />, href: "https://github.com/NitanJana" },
-  { icon: <FaLinkedin />, href: "https://www.linkedin.com/in/nitan-jana/" },
-  { icon: <FaTwitter />, href: "https://x.com/Nitan_Official" },
+  { icon: <FaGithub />, href: "https://github.com/NitanJana", label: "Github" },
+  {
+    icon: <FaLinkedin />,
+    href: "https://www.linkedin.com/in/nitan-jana/",
+    label: "LinkedIn",
+  },
+  {
+    icon: <FaTwitter />,
+    href: "https://x.com/Nitan_Official",
+    label: "Twitter",
+  },
 ];
 
 const Social = ({ containerStyles = "", iconStyles = "" }) => {
@@ -14,6 +22,7 @@ const Social = ({ containerStyles = "", iconStyles = "" }) => {
         <Link
           key={item.href}
           href={item.href}
+          aria-label={item.label}
           target="_blank"
           rel="noopener noreferrer"
           className={iconStyles}
